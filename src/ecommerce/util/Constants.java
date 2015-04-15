07de083f4f87f3ba3ced250521cbe0eec662ea3f
gil.olaes@gmail.com <gil.olaes@gmail.com>
2015-04-14 17:19:21 -0700

    Queries

--- src/ecommerce/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package ecommerce.util;
-
-public final class Constants {
-	public static final String m_username="kevingu";
-	public static final String m_password="";
-	public static final String m_dbms="test";
-	public static final String m_serverName="localhost";
-	public static final String m_portNumber="5432";
-}

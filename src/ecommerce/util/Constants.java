37920062aff35dc713158f45f93f3aa1cc1c23a5
gil.olaes@gmail.com <gil.olaes@gmail.com>
2015-04-17 16:27:51 -0700

    Gil's Constants

--- src/ecommerce/util/Constants.java
+++ src/ecommerce/util/Constants.java
@@ -1,12 +1,12 @@
 package ecommerce.util;
 
 public final class Constants {
-	public static final String m_username="kevingu";
-	public static final String m_password="";
+	public static final String m_username="postgres";
+	public static final String m_password="Okgo1199";
 	public static final String m_dbms="postgresql";
 	public static final String m_serverName="localhost";
 	public static final String m_portNumber="5432";
-	public static final String m_db="test";
+	public static final String m_db="postgres";
 	public static final String CUSTOMER="Customer";
 	public static final String OWNER="Owner";
 }
